4ea96bd7aca0879bf102b7487990f1174210532f
andres venegas <afvenegas10@hotmail.com>
2015-09-26 12:43:28 -0500

    lib

--- Ad/src/com/ad/MainActivity.java
+++ Ad/src/com/ad/MainActivity.java
@@ -1,10 +1,9 @@
 package com.ad;
 
-//import android.support.v7.app.AppCompatActivity;
-
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
